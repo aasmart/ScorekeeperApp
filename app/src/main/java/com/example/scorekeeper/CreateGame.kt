60c78d2e6a0694f752226687ed04e20f99034b64
NotSmart <48771274+aasmart@users.noreply.github.com>
2023-04-16 09:33:51 -0400

    Redid the create new game menu

---
+++ app/src/main/java/com/example/scorekeeper/CreateGame.kt
@@ -0,0 +1,370 @@
+package com.example.scorekeeper
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.ExposedDropdownMenuDefaults
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.scorekeeper.ui.theme.Purple500
+import com.example.scorekeeper.ui.theme.Purple700
+
+class CreateGameForm(private val appViewModel: AppViewModel) {
+    private val gameName = mutableStateOf("")
+    private val playerName = mutableStateOf("")
+    private val players = mutableStateListOf<String>()
+    private val selectedIndex = mutableStateOf(0)
+
+    private val INPUT_FIELD_HEIGHT = 56.dp
+    private val PLAYER_LIST_HEIGHT = 280.dp
+
+    fun isValidLength(str: String, min: Int, max: Int = Int.MAX_VALUE): Boolean {
+        return str.length in min..max
+    }
+
+    @Composable
+    private fun TopAppBar() {
+        TopAppBar(
+            backgroundColor = Purple700,
+            elevation = 8.dp,
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    IconButton(
+                        onClick = { appViewModel.toggleGameModal() },
+                        modifier = Modifier.size(32.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White,
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    TitleText(stringResource(R.string.create_new_game))
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun FinishGameButton() {
+        val context = LocalContext.current
+        FloatingActionButton(
+            onClick = {
+                if (!isValidLength(gameName.value, 1, 24)) {
+                    Toast.makeText(context, "Invalid game name", Toast.LENGTH_SHORT).show()
+                    return@FloatingActionButton
+                }
+
+                val minPlayers = AppViewModel.ScoringType.values()[selectedIndex.value].minPlayers
+                if (players.size < minPlayers) {
+                    Toast.makeText(
+                        context,
+                        "This game requires at least $minPlayers player ${if (minPlayers == 1) "" else "s"}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@FloatingActionButton
+                }
+
+                appViewModel.addNewGame(
+                    gameName.value,
+                    players.toList(),
+                    AppViewModel.ScoringType.values()[selectedIndex.value]
+                )
+            },
+            backgroundColor = Purple500,
+        ) {
+            Icon(Icons.Filled.Check, "Game")
+        }
+    }
+
+    @OptIn(
+        ExperimentalMaterialApi::class
+    )
+    @Composable
+    private fun GameTypeDropdown() {
+        val context = LocalContext.current
+
+        val expanded = remember { mutableStateOf(false) }
+
+        ExposedDropdownMenuBox(
+            expanded = expanded.value,
+            onExpandedChange = {
+                expanded.value = !expanded.value
+            },
+            modifier = Modifier.height(INPUT_FIELD_HEIGHT)
+        ) {
+            TextField(
+                value = AppViewModel.ScoringType.values()[selectedIndex.value].readableName,
+                onValueChange = {},
+                label = { Text(text = stringResource(R.string.game_preset)) },
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = expanded.value
+                    )
+                },
+                modifier = Modifier.fillMaxSize()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded.value,
+                onDismissRequest = { expanded.value = false },
+            ) {
+                AppViewModel.ScoringType.values().forEachIndexed { index, enum ->
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedIndex.value = index
+                            expanded.value = !expanded.value
+                            Toast.makeText(
+                                context,
+                                AppViewModel.ScoringType.values()[selectedIndex.value].readableName,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(enum.readableName)
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun Player(
+        name: String,
+        context: Context,
+        players: MutableList<String>,
+        playerName: String
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colors.background, shape = RoundedCornerShape(8))
+                .padding(4.dp)
+                .clickable {
+                    Toast
+                        .makeText(context, "Removed '${playerName}'", Toast.LENGTH_SHORT)
+                        .show()
+                    players.remove(name)
+                }
+        ) {
+            Text(text = name, fontSize = 18.sp, modifier = Modifier.weight(1f, true))
+            Icon(Icons.Filled.Remove, "Remove player", tint = MaterialTheme.colors.error)
+        }
+    }
+
+    @Composable
+    private fun PlayerInput() {
+        val context = LocalContext.current
+
+        Card(
+            shape = RoundedCornerShape(3.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(PLAYER_LIST_HEIGHT),
+            elevation = 4.dp,
+            border = BorderStroke(1.dp, MaterialTheme.colors.background),
+        ) {
+            // PLAYER NAME LIST
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(6.dp),
+                contentPadding = PaddingValues(12.dp)
+
+            ) {
+                items(players.toList()) { player ->
+                    Player(
+                        player,
+                        context,
+                        players,
+                        playerName.value
+                    )
+                }
+            }
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Max)
+        ) {
+            fun addPlayerToGame() {
+                if (!isValidLength(playerName.value, 1, 24)) {
+                    Toast.makeText(
+                        context,
+                        "Name must be within valid size.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+
+                if (players.contains(playerName.value)) {
+                    Toast.makeText(
+                        context,
+                        "There is already a player with this name.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
+
+                players.add(playerName.value)
+                Toast.makeText(
+                    context,
+                    "Added player '${playerName.value}'",
+                    Toast.LENGTH_SHORT
+                ).show()
+                playerName.value = ""
+            }
+
+            TextField(
+                value = playerName.value,
+                onValueChange = { playerName.value = it },
+                label = { Text(text = stringResource(R.string.player_name_field)) },
+                singleLine = true,
+                maxLines = 1,
+                isError = !isValidLength(playerName.value, 1, 24),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    addPlayerToGame()
+                }),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(5f, true)
+            )
+
+            Button(
+                onClick = { addPlayerToGame() },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Purple500,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .weight(1f, true)
+            ) {
+                Icon(
+                    Icons.Filled.Add,
+                    "Finish creating new game",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+
+    // TODO General cleanup, improve length function, fix keyboard overlap,
+    @Composable
+    fun Modal() {
+        Scaffold(
+            topBar = { TopAppBar() },
+            floatingActionButton = {
+                FinishGameButton()
+            },
+            bottomBar = {
+                BottomAppBar(
+                    backgroundColor = Purple700
+                ) {
+
+                }
+            },
+            floatingActionButtonPosition = FabPosition.Center,
+            isFloatingActionButtonDocked = true
+        ) {
+            Box(modifier = Modifier.padding(it)) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .fillMaxSize()
+                        .padding(20.dp),
+                ) {
+                    TextField(
+                        value = gameName.value,
+                        onValueChange = { gameName.value = it },
+                        label = { Text(text = stringResource(R.string.game_name)) },
+                        singleLine = true,
+                        isError = !isValidLength(gameName.value, 1, 24),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(INPUT_FIELD_HEIGHT),
+                    )
+
+                    GameTypeDropdown()
+
+                    // PLAYER INPUT
+                    Text(text = "Players", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+                    Divider()
+
+                    PlayerInput()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/scorekeeper/MainActivity.kt
+++ app/src/main/java/com/example/scorekeeper/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.scorekeeper
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -27,6 +21,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.scorekeeper.game.types.Game
 import com.example.scorekeeper.ui.theme.CAHAppTheme
@@ -46,6 +43,12 @@ class MainActivity : ComponentActivity() {
                     AppMain()
                 }
             }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { view, insets ->
+            val bottom = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+            view.updatePadding(bottom = bottom)
+            insets
         }
     }
 }
@@ -96,8 +99,21 @@ fun AppMain(appViewModel: AppViewModel = viewModel()) {
         }
     }
 
-    if (appUiState.isCreatingGame)
-        NewGameModal(appViewModel)
+    AnimatedVisibility(
+        visible = appUiState.isCreatingGame,
+        enter = slideInHorizontally() {
+                maxWidth -> maxWidth / 3
+        } + fadeIn(
+            // Fade in with the initial alpha of 0.3f.
+            initialAlpha = 0.3f
+        ),
+        exit = slideOutHorizontally() { maxWidth -> maxWidth / 3 } + fadeOut()
+    ) {
+        BackHandler(enabled = true) {
+            appViewModel.toggleGameModal();
+        }
+        CreateGameForm(appViewModel).Modal()
+    }
 
     AnimatedVisibility(
         visible = appUiState.isFocusedGameVisible,
@@ -150,242 +166,6 @@ fun NewGameButton(addGame: () -> Unit) {
         backgroundColor = Purple500,
     ) {
         Icon(Icons.Filled.Add, "Game")
-    }
-}
-
-// TODO General cleanup, improve length function, fix keyboard overlap,
-@OptIn(
-    ExperimentalMaterialApi::class
-)
-@Composable
-fun NewGameModal(appViewModel: AppViewModel) {
-    val context = LocalContext.current
-
-    // The name for the new game being created
-    val gameName = remember { mutableStateOf("") }
-    val playerName = remember { mutableStateOf("") }
-    val players = remember { mutableStateListOf<String>() }
-    val expanded = remember { mutableStateOf(false) }
-    val selectedIndex = remember { mutableStateOf(0) }
-
-    @Composable
-    fun Player(name: String) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = MaterialTheme.colors.background, shape = RoundedCornerShape(8))
-                .padding(4.dp)
-                .clickable {
-                    Toast
-                        .makeText(context, "Removed '${playerName.value}'", Toast.LENGTH_SHORT)
-                        .show()
-                    players.remove(name)
-                }
-        ) {
-            Text(text = name, fontSize = 18.sp, modifier = Modifier.weight(1f, true))
-            Icon(Icons.Filled.Remove, "Remove player", tint = MaterialTheme.colors.error)
-        }
-
-    }
-
-    fun isValidName(str: String): Boolean {
-        return str.length in 1..24
-    }
-
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color(0, 0, 0, 80))
-        .clickable(
-            interactionSource = MutableInteractionSource(),
-            indication = null
-        ) { appViewModel.toggleGameModal() }, contentAlignment = Alignment.Center
-    ) {
-        Card(
-            shape = RoundedCornerShape(10.dp),
-            elevation = 8.dp,
-            modifier = Modifier
-                .padding(30.dp, 5.dp, 30.dp, 10.dp)
-                .heightIn(0.dp, 680.dp)
-                .clickable(interactionSource = MutableInteractionSource(), indication = null) {},
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.padding(16.dp),
-            ) {
-                // Title
-                Text(
-                    text = stringResource(R.string.create_new_game),
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Black
-                )
-                Divider()
-
-                // Input fields
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(6.dp)
-                ) {
-
-                    TextField(
-                        value = gameName.value,
-                        onValueChange = { gameName.value = it },
-                        label = { Text(text = stringResource(R.string.game_name)) },
-                        singleLine = true,
-                        isError = !isValidName(gameName.value),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(10f, true),
-                    )
-
-                    Box(modifier = Modifier
-                        .weight(10f, true)
-                        .fillMaxWidth()) {
-                        ExposedDropdownMenuBox(
-                            expanded = expanded.value,
-                            onExpandedChange = {
-                                expanded.value = !expanded.value
-                            }
-                        ) {
-                            TextField(
-                                value = AppViewModel.ScoringType.values()[selectedIndex.value].readableName,
-                                onValueChange = {},
-                                label = { Text(text = stringResource(R.string.game_preset)) },
-                                readOnly = true,
-                                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded.value) },
-                                modifier = Modifier.fillMaxWidth()
-                            )
-
-                            ExposedDropdownMenu(
-                                expanded = expanded.value,
-                                onDismissRequest = { expanded.value = false },
-                                modifier = Modifier.width(IntrinsicSize.Max)
-                            ) {
-                                AppViewModel.ScoringType.values().forEachIndexed { index, enum ->
-                                    DropdownMenuItem(onClick = {
-                                        selectedIndex.value = index
-                                        expanded.value = !expanded.value
-                                        Toast.makeText(context, AppViewModel.ScoringType.values()[selectedIndex.value].readableName, Toast.LENGTH_SHORT).show()
-                                    }) {
-                                        Text(enum.readableName)
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    // PLAYER INPUT
-                    Text(text = "Players", fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.weight(5f, true))
-                    Divider()
-                    Card(
-                        shape = RoundedCornerShape(3.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(55f, true)
-                            .height(200.dp),
-                        elevation = 4.dp,
-                        border = BorderStroke(1.dp, MaterialTheme.colors.background),
-                    ) {
-                        // PLAYER NAME LIST
-                        LazyColumn(
-                            verticalArrangement = Arrangement.spacedBy(6.dp),
-                            contentPadding = PaddingValues(12.dp)
-
-                        ) {
-                            items(players.toList()) { player -> Player(player) }
-                        }
-                    }
-
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(10f, true)
-                    ) {
-                        fun addPlayerToGame() {
-                            if(!isValidName(playerName.value))  {
-                                Toast.makeText(context, "Name must be within valid size.", Toast.LENGTH_SHORT).show()
-                                return
-                            }
-
-                            if(players.contains(playerName.value)) {
-                                Toast.makeText(context, "There is already a player with this name.", Toast.LENGTH_SHORT).show()
-                                return
-                            }
-
-                            players.add(playerName.value)
-                            Toast.makeText(context, "Added player '${playerName.value}'", Toast.LENGTH_SHORT).show()
-                            playerName.value = ""
-                        }
-
-                        TextField(
-                            value = playerName.value,
-                            onValueChange = { playerName.value = it },
-                            label = { Text(text = stringResource(R.string.player_name_field)) },
-                            singleLine = true,
-                            maxLines = 1,
-                            isError = !isValidName(playerName.value),
-                            keyboardActions = KeyboardActions(onDone = {
-                                addPlayerToGame()
-                            }),
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(8f, true)
-                        )
-
-                        Button(
-                            onClick = { addPlayerToGame() },
-                            colors = ButtonDefaults.buttonColors(backgroundColor = Purple500, contentColor = Color.White),
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .weight(2f, true)
-                        ) {
-                            Icon(Icons.Filled.Add, "Add Player", modifier = Modifier.fillMaxSize())
-                        }
-                    }
-
-                    // Button bar
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(10f, true),
-                        horizontalArrangement = Arrangement.spacedBy(12.dp)
-                    ) {
-                        // Cancel button
-                        OutlinedButton(
-                            onClick = { appViewModel.toggleGameModal() },
-                            modifier = Modifier.weight(1f),
-                            elevation = ButtonDefaults.elevation(defaultElevation = 2.dp)
-                        ) {
-                            Text(text = stringResource(R.string.cancel))
-                        }
-
-                        // Continue button
-                        Button(
-                            onClick = {
-                                if(!isValidName(gameName.value)) {
-                                    Toast.makeText(context, "Invalid game name", Toast.LENGTH_SHORT).show()
-                                    return@Button
-                                }
-
-                                val minPlayers = AppViewModel.ScoringType.values()[selectedIndex.value].minPlayers
-                                if(players.size < minPlayers) {
-                                    Toast.makeText(context, "This game requires at least $minPlayers player ${if(minPlayers == 1) "" else "s"}", Toast.LENGTH_SHORT).show()
-                                    return@Button
-                                }
-
-                                appViewModel.addNewGame(gameName.value, players.toList(), AppViewModel.ScoringType.values()[selectedIndex.value])
-                            },
-                            elevation = ButtonDefaults.elevation(defaultElevation = 2.dp),
-                            modifier = Modifier.weight(1f)
-                        ) {
-                            Text(text = stringResource(R.string.create_game))
-                        }
-                    }
-                }
-            }
-        }
     }
 }
 
